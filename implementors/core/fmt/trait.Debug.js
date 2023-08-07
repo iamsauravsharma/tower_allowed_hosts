fc4259f1ea3ecf8a6c2c284006b833511c0bc6c5
iamsauravsharma <appdroiddeveloper@gmail.com>
2023-08-07 13:58:43 +0000

    Deploying to gh-pages from @ iamsauravsharma/tower_allowed_hosts@e422a8c29a195b3ab4b59a39877e6f0e5360c2db 🚀

---
+++ implementors/core/fmt/trait.Debug.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"tower_allowed_hosts":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/fmt/trait.Debug.html\" title=\"trait core::fmt::Debug\">Debug</a> for <a class=\"enum\" href=\"tower_allowed_hosts/enum.Error.html\" title=\"enum tower_allowed_hosts::Error\">Error</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ src-files.js
@@ -0,0 +1,4 @@
+var srcIndex = JSON.parse('{\
+"tower_allowed_hosts":["",[],["lib.rs"]]\
+}');
+createSrcSidebar();
