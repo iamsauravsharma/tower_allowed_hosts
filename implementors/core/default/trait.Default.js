02244b0a1c31be4ad6548ffe0de8c3c0fe509d93
iamsauravsharma <appdroiddeveloper@gmail.com>
2023-08-08 05:21:00 +0000

    Deploying to gh-pages from @ iamsauravsharma/tower_allowed_hosts@46bd10f2a3f395e0e034bcf7ac944e7a28acab36 🚀

---
+++ implementors/core/default/trait.Default.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {
+"tower_allowed_hosts":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/default/trait.Default.html\" title=\"trait core::default::Default\">Default</a> for <a class=\"struct\" href=\"tower_allowed_hosts/struct.AllowedHostLayer.html\" title=\"struct tower_allowed_hosts::AllowedHostLayer\">AllowedHostLayer</a>"]]
+};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
