1ba94a97353f9c27e32078495a32933a5592eed6
Saurav Sharma <appdroiddeveloper@gmail.com>
2024-09-10 19:59:37 +0545

    feat!: update supported tower version to 0.5.0
    Signed-off-by: Saurav Sharma <appdroiddeveloper@gmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -12,10 +12,11 @@ pub mod service;
 #[cfg(test)]
 mod tests;
 
-/// Struct which holds value of host along with its port if present which was
-/// parsed and allowed by allowed host layer. This struct is added as a
-/// extension to request after successfully resolving host and verifying host is
-/// valid host which can be used in server if needed for further uses
+/// Struct which holds value of host along with its port.
+///
+/// This struct is added as a extension to request after successfully resolving
+/// host and verifying host is valid host which can be used in server if needed
+/// for further uses
 #[derive(Clone)]
 pub struct Host(pub String);
 
