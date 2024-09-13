a41c42b72a374457f45eb1ce63323b4191d3604c
iamsauravsharma <appdroiddeveloper@gmail.com>
2024-09-13 16:37:25 +0000

    Deploying to gh-pages from @ iamsauravsharma/tower_allowed_hosts@70fd67f8b6ae5a8f003f5ffa74d861d43cd5d7fb 🚀

--- trait.impl/tower_service/trait.Service.js
+++ trait.impl/tower_service/trait.Service.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["tower_allowed_hosts",[["impl&lt;S, ReqBody, ResBody&gt; Service&lt;Request&lt;ReqBody&gt;&gt; for <a class=\"struct\" href=\"tower_allowed_hosts/service/struct.AllowedHost.html\" title=\"struct tower_allowed_hosts::service::AllowedHost\">AllowedHost</a>&lt;S&gt;<div class=\"where\">where\n    S: Service&lt;Request&lt;ReqBody&gt;, Response = Response&lt;ResBody&gt;&gt;,\n    S::Error: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;BoxError&gt;,</div>"]]]]);
+    var implementors = Object.fromEntries([["tower_allowed_hosts",[["impl&lt;S, ReqBody&gt; Service&lt;Request&lt;ReqBody&gt;&gt; for <a class=\"struct\" href=\"tower_allowed_hosts/service/struct.AllowedHost.html\" title=\"struct tower_allowed_hosts::service::AllowedHost\">AllowedHost</a>&lt;S&gt;<div class=\"where\">where\n    S: Service&lt;Request&lt;ReqBody&gt;&gt;,\n    S::Error: <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/convert/trait.Into.html\" title=\"trait core::convert::Into\">Into</a>&lt;BoxError&gt;,</div>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[551]}
+//{"start":57,"fragment_lengths":[506]}
