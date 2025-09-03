20c2b12f1f15e6314c2b310f9dc64002da7797bc
iamsauravsharma <appdroiddeveloper@gmail.com>
2025-09-03 12:28:24 +0000

    Deploying to gh-pages from @ iamsauravsharma/tower_allowed_hosts@3407fb89a1df3fc880341d91ead8d82fe7beff6e 🚀

--- tower_allowed_hosts/sidebar-items.js
+++ tower_allowed_hosts/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":["error","matcher","service"],"struct":["AllowedHostLayer","Host"]};
+window.SIDEBAR_ITEMS = {"enum":["Error"],"mod":["error","matcher","service"],"struct":["AllowedHostLayer","Host"]};
